b961f72c905ad6284ee862e0c7b8694fbecb8794
Fanny Buhlér <fanny.buhler@hyperisland.se>
2021-04-22 14:20:31 +0200

    json placeholder

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,6 +1,31 @@
+
 <template>
-  <div>
-    <h1>News Page</h1>
-    <NuxtLink to="/">Home</NuxtLink>
+  <p v-if="$fetchState.pending">Fetching posts...</p>
+  <p v-else-if="$fetchState.error">An error occurred :(</p>
+  <div v-else>
+    <h1>Nuxt posts</h1>
+    <ul>
+      <li v-for="post of posts" :key="post.title">
+        <NuxtLink :to="`/news/${post.id}`">
+          {{ post.id }}. {{ post.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+    <button @click="$fetch">Refresh</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    }
+  },
+  async fetch() {
+    this.posts = await fetch(
+      'https://jsonplaceholder.typicode.com/posts'
+    ).then((res) => res.json())
+  },
+}
+</script>
